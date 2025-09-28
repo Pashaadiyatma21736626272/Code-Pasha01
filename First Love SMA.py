fd68732b42d18977bbeb54e18393af70ebf412b5
Pasha_adiyatma <pashaadiyatma101007@gmail.com>
2025-09-28 10:42:23 +0700

    0.4

--- First Love SMA.py
+++ First Love SMA.py
@@ -1,3 +1,7 @@
 print("Siapa Cinta Sejatimu: ")
 print("--------------------")
 print("Laura Fila Delfia Br Milala")
+
+#Input Jodoh Laura
+nama = input("Masukkan Namamu : ")
+print("Pasha Love Laura")
